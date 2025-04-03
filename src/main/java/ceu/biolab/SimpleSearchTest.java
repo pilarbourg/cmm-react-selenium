59185bcac6b9db6e24f4e82b287bf08a118762da
pilarbourg <pilar.bourg@usp.ceu.es>
2025-04-03 08:57:51 +0200

    added junit to pom.xml

--- src/main/java/ceu/biolab/SimpleSearchTest.java
+++ src/main/java/ceu/biolab/SimpleSearchTest.java
@@ -11,19 +11,22 @@ import org.openqa.selenium.JavascriptExecutor;
 import java.util.*;
 
 public class SimpleSearchTest {
-  private WebDriver driver;º  º
+  private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
+
   @Before
   public void setUp() {
     driver = new FirefoxDriver();
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
   }
+
   @After
   public void tearDown() {
     driver.quit();
   }
+
   @Test
   public void simplesearch() {
     driver.get("https://localhost:5178/simple-search");
